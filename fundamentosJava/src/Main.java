546da9a149b491ff79dbf8d718c348ed7ed09408
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-04-07 14:35:59 -0300

    Hello word para nao perder o custume

--- fundamentosJava/src/Main.java
+++ fundamentosJava/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+
+    /*Para nao sugir a regra vamos escrever o Hello Word! como a pag 12 da apostila Java e POO da caelum pede.*/
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! My name is Nicolle Gianinni! It's my frist java application seeking Java certificate!");
     }
 }
