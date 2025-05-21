01881f7cdd963473c4dc8d951550be2b9f71f93d
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-05-20 22:21:40 -0300

    for each

--- fundamentosJava/src/arrays/ForEach.java
+++ fundamentosJava/src/arrays/ForEach.java
@@ -1,18 +1,66 @@
 package arrays;
 
+import java.util.Random;
+
 public class ForEach {
     // Bastante usado nos arrays
 
     public static void main(String[] args) {
 
         System.out.println("For tradional");
-        int [] notas = new int [10];
-        for (int i =0; i < notas.length;i++){
+        int[] notas = new int[10];
+        for (int i = 0; i < notas.length; i++) {
             System.out.println(notas[i]);
         }
         System.out.println("For each");
-        for(int nota : notas){
+
+        for (int nota : notas) {
             System.out.println(nota);
+        }
+
+        System.out.println("usando randon");
+        Random randon = new Random();
+        int[] nootas = new int[10];
+        for (int i = 0; i < nootas.length; i++) {
+            nootas[i] = randon.nextInt();
+
+        }
+        for (int i = 0; i < notas.length; i++) {
+            System.out.println(nootas[i]);
+        }
+
+        System.out.println();
+        for (int noota : nootas) {
+            System.out.println(noota);
+        }
+
+        // exemplo 2
+
+        int[][] notasAlunos = new int[3][3];
+
+        notasAlunos[0][0] = 10;
+        notasAlunos[0][1] = 8;
+        notasAlunos[0][2] = 7;
+
+        notasAlunos[1][0] = 6;
+        notasAlunos[1][1] = 10;
+        notasAlunos[1][2] = 8;
+
+        notasAlunos[2][0] = 8;
+        notasAlunos[2][1] = 8;
+        notasAlunos[2][2] = 8;
+
+        System.out.println();
+
+        // for each
+
+        for (int[] notaAluno : notasAlunos) {
+            for ( int  Nota : notaAluno) {
+                System.out.print(Nota + " ");
+            }
+            System.out.println();
         }
     }
 }
+
+
