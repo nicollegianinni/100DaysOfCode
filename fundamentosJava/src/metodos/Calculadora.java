82f281267725afe9d1f3aa9886ac79ad13ab2adb
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-06-17 10:52:02 -0300

    return

--- fundamentosJava/src/metodos/Calculadora.java
+++ fundamentosJava/src/metodos/Calculadora.java
@@ -2,15 +2,25 @@ package metodos;
 
 public class Calculadora {
 
-    public void somaDoisNumeros(){
-        System.out.println(10+10);
+    public void somaDoisNumeros() {
+        System.out.println(10 + 10);
     }
 
-    public void subtraiDoisNumeros(){
+    public void subtraiDoisNumeros() {
         System.out.println(10 - 5);
     }
 
-    public void multiplicaDoisNumeros (int num1, int num2){
-        System.out.println(num1*num2);
+    public void multiplicaDoisNumeros(int num1, int num2) {
+        System.out.println(num1 * num2);
+    }
+
+    public double divideDoisNumeros(double num1, double num2) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
     }
 }
+
+
--- fundamentosJava/src/metodos/test/CalculadoraTest.java
+++ fundamentosJava/src/metodos/test/CalculadoraTest.java
@@ -15,5 +15,22 @@ public class CalculadoraTest {
         System.out.println();
         System.out.println("Testando a multiplicação");
         calculadora.multiplicaDoisNumeros(10,2);
+
+        //diferente dos metodos void sem retorno, esse precisa salvar em uma variavel ou explicitar no sout(saida)
+//       assim nao saida dados nenhum:
+//        System.out.println("Testando a divisão");
+//        calculadora.divideDoisNumeros(10,2);
+//        System.out.println();
+//        System.out.println("Testando o erro da divisão por zero");
+//        calculadora.divideDoisNumeros(10,0);
+//        System.out.println();
+        // vamos ver a maneira correta:
+        System.out.println();
+        System.out.println("Testando a divisão");
+        double result = calculadora.divideDoisNumeros(10,2);
+        System.out.println(result);
+        System.out.println("Testando o erro da divisão por zero");
+        double resultado = calculadora.divideDoisNumeros(10,0);
+        System.out.println(resultado);
     }
 }
