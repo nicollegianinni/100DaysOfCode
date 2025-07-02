f983e91372f5f3131a755d5c308bd2c9dec6cede
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-07-02 16:16:25 -0300

    enum para busca por atributo

--- fundamentosJava/src/Enum/ClienteTeste2.java
+++ fundamentosJava/src/Enum/ClienteTeste2.java
@@ -14,14 +14,24 @@ public class ClienteTeste2 {
         System.out.println(cliente3);
         System.out.println(cliente4);
 
+        //SAIDA ESPERADA: Sera impresso "Com a classe Enum{nome,CPF,tipo} do metodo toString() da classe (ClienteClasse2)
+        //como adicionamos um Enum 'TipoPagamento' na propria ClienteClasse2, e adicionamos no toString() devera sair todos os dados do objeto criado.
+        //Depois de testar os anteriores, criou se um novo Enum 'TipoContaEnum' com parametros (tipoConta, numeroTipoConta e qual tipo da conta corrente Pj ou poupança)
+        //Depois de testar anteriores, cria se outro Enum 'CategoriaClienteEnum'
 
         //Exemplo da Classe Enum 'CategoriaClienteEnum' metodo calcularDesconto() Cliente OURO E PRATA
         System.out.println(CategoriaClienteEnum.CLIENTE_OURO.calculeDesconto(100));
         System.out.println(CategoriaClienteEnum.CLIENTE_PRATA.calculeDesconto(100));
 
-        //SAIDA ESPERADA: Sera impresso "Com a classe Enum{nome,CPF,tipo} do metodo toString() da classe (ClienteClasse2)
-        //como adicionamos um Enum 'TipoPagamento' na propria ClienteClasse2, e adicionamos no toString() devera sair todos os dados do objeto criado.
-        //Depois de testar os anteriores, criou se um novo Enum 'TipoContaEnum' com parametros (tipoConta, numeroTipoConta e qual tipo da conta corrente Pj ou poupança)
-        //Depois de testar anteriores, cria se outro Enum 'CategoriaClienteEnum'
+        //REFERENTE A CLASSE ENUM: ' TipoContaEnum' Para fazer a busca por atributitos
+        TipoContaEnum tipoContaEnum = TipoContaEnum.valueOf("CONTA_CORRENTE");
+        System.out.println(tipoContaEnum.getNomeRelatorio());
+//passando parametro corerreto do nomerelatorio da classe Enum 'TipoContaEnum'
+        TipoContaEnum tipoContaEnum2 = TipoContaEnum.tipoContaPorNomeRelatorio("Conta Poupança");
+        System.out.println(tipoContaEnum2);
+// se passar o parametro errado volta null
+        TipoContaEnum tipoContaEnum3 = TipoContaEnum.tipoContaPorNomeRelatorio("Conta Poupanc");
+        System.out.println(tipoContaEnum3);
+
     }
 }
