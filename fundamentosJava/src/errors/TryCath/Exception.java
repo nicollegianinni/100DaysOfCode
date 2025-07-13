b99c38ec242024b3ca9c87580aa3c1d2cf599e01
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-07-13 15:30:24 -0300

    try cath mutiplos

--- fundamentosJava/src/errors/TryCath/Exception.java
+++ fundamentosJava/src/errors/TryCath/Exception.java
@@ -12,19 +12,20 @@ public class Exception {
 //        vetor[4]=1; // Erro só eixstem posições {0,1,2,3} não existe {4}: java.lang: ArrayIndexOutOfBoundsException
 //        System.out.println("Esse texto nao será impresso, pois é apos o erro.");
 
-        //comente o primeiro exemplo para testar o 2
-        //o primeiro exemplo interrompe o codigo
-        System.out.println("-------------------");
+        //descomente o primeiro exemplo para testa-lo -
+        // mostrara o erro sem defini onde é nem o que houve e nao contuinuara o codigo
+        //o primeiro exemplo interrompe, o codigo nao roda nada apos
+
 
         //EXEMPLO 2
         System.out.println("Novo exemplo");
         int[] vetor2 = new int[4];
         try {
-            vetor2[4]=1;
-        }catch (java.lang.Exception e){
-            System.out.println("Essa posição não existe no array!"); // mensagem amigavel do erro
-            e.printStackTrace(); //por ultimo, mostrara onde esta o erro
-            System.out.println(e.getMessage()); // mostrara uma mensagem mostrando qual erro
+            vetor2[4]=1;   // Erro só eixstem posições {0,1,2,3} não existe {4}: java.lang: ArrayIndexOutOfBoundsException
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("EXCEPTION: Essa posição não existe no array!"); // MOSTRAR PARA USUARIOS- mensagem amigavel do erro
+            //e.printStackTrace(); //MOSTRAR P DESENVOLVEDORES- por ultimo mostrara onde esta o erro
+            //System.out.println(e.getMessage()); // //MOSTRAR P DESENVOLVEDORES -mostrara uma mensagem mostrando qual erro
         }
         //mostrar essa mensagem continuando o codigo
         System.out.println("Agora mostrar essa mensagem pois o codigo nao é interrompido" +
