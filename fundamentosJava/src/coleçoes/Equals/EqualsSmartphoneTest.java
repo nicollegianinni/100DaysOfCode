9c02303b5991065c2eac666ecc06265d85155fbd
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-08-13 19:52:48 -0300

    equals comprarando strings

--- fundamentosJava/src/coleçoes/Equals/EqualsSmartphoneTest.java
+++ fundamentosJava/src/coleçoes/Equals/EqualsSmartphoneTest.java
@@ -7,12 +7,17 @@ public class EqualsSmartphoneTest {
         Smartphone s1 = new Smartphone("123456", "Samsung");
         Smartphone s2 = new Smartphone("123456", "Samsung");
 
-
-        //observando equals para espaço de memoria
+        //1 - OBSERVE QUE ELE COMPARA O ESPAÇO DE MEMORIA DOS OBJETOS ATRAVES DO EQUALS
         System.out.println(s1.equals(s2)); // false pois o espaço de memoria é diferente ja que são 2 objetos distintos
 
-        s1 = s2; // ao atrubuir um objeto ao outro é tranferido o espaço de memoria
+        s1 = s2; // ao atrubuir um objeto ao outro é tranferido o ESPAÇO DE MEMORIA ( não compara o que tem dnetro do objeto)
         System.out.println(s1.equals(s2)); // agora a saida é true
+
+
+        // 2 PARA VER COMPARANDO O QUE TEM DENTRO DO OBJETO, EX.: para ver se o 'SerialNumber' do s1 é igual ao 'SerialNumber' do s2
+        // VA NA CLASSE 'Smatphone' e DESCOMENTE o metodo equals 'public boolean equals(Object obj)' da classe
+
+
 
 
 
--- fundamentosJava/src/coleçoes/Equals/Smartphone.java
+++ fundamentosJava/src/coleçoes/Equals/Smartphone.java
@@ -10,6 +10,23 @@ public class Smartphone {
         this.marca = marca;
     }
 
+    //METODO EQUALS:  compara atrubutos dentro de um objeto
+    //REGRAS:
+    // reflexivo: -> x.equals(x) tem que ser true para  tudo que for diferente de null
+    //simetrico: para x e y diferente de null, se x.equals(y) == true, logo y.equals(x) == true
+    //transitividade: para x e y diferentes de null, se x.equals(y) == true, logo x.equals(z) == true, logo y.equals(z) == true
+    //consistente: x.equals(x) ele sempre retorna true se x for diferente de null
+    //para x diferente de null, x.equals(null) tem que retornar false
+    //DESCOMENTA ESSE CODIGO ABAIXO PARA COMPARAR O QUE TEM DENTRO DO OBJETO ATRAVES DO EQUALS
+//    @Override
+//    public boolean equals(Object obj) {
+//        if (obj == null) return false;
+//        if (this == obj) return true;
+//        if (this.getClass() != obj.getClass()) return false;
+//        Smartphone smartphone = (Smartphone) obj; // faz um cast para ter certeza que o objeto é um smartphone
+//        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+//    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
