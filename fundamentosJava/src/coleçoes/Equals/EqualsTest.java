6d62b36be45966baa41edaf78a34a3f63b1a79e8
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-08-23 16:53:52 -0300

    string

--- fundamentosJava/src/coleçoes/Equals/EqualsTest.java
+++ fundamentosJava/src/coleçoes/Equals/EqualsTest.java
@@ -4,7 +4,7 @@ public class EqualsTest {
     public static void main(String[] args) {
         String nome1 = "William Suane";
         String nome2 = "William Suane";
-        System.out.println(nome2 == nome2); // true pq faz referencia o mesmo espaço de memoria
+        System.out.println(nome1 == nome2); // true pq ele compara a variavel de referencia o nome "william Suane".
 
         String nome3 = new String("Willian Suane");
         System.out.println(nome2 == nome3); // false pois  como criou um objeto novo fez um novo espaço de memoria
