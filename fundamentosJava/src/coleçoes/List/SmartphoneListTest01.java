8758ce743eb4767b7caa423b0cbfad144c412f0a
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-08-23 22:37:43 -0300

    List com objetos

---
+++ fundamentosJava/src/coleçoes/List/SmartphoneListTest01.java
@@ -0,0 +1,11 @@
+package coleçoes.List;
+
+import coleçoes.Equals.Smartphone;
+
+//USANDO LISTA COM OBJETOS
+public class SmartphoneListTest01 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("123456", "Samsung");
+        Smartphone s2 = new Smartphone("123456", "Samsung");
+    }
+}
