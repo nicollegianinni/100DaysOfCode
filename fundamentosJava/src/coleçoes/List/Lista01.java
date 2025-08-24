4a3e3f14ab3fe1c5d5ccd468b0fa621bef7f0bf8
NICHOLLE GIANINNI <gianinni.nicolle@gmail.com>
2025-08-23 22:35:09 -0300

    List arrayList

--- fundamentosJava/src/coleçoes/List/Lista01.java
+++ fundamentosJava/src/coleçoes/List/Lista01.java
@@ -21,22 +21,79 @@ public class Lista01 {
 
         System.out.println("-----------------Nova forma de fazer----------------------");
         // DEPOIS DO GENERICS NO JAVA 5 PRECISA INTRODUZIR O TIPO ATRAVES DO DIAMOND <>
+        //NÃO PODE CRIAR NENHUMA LISTA DE VARIAVEL PRIMITIVA TEM QUE SER OBJETO USA O WRAPPER ex.: int, double; tem que ser Integer, Double
+        // internamente chama o equals e hashCode e esses não chamam tipos primitivos
 //Pode usar ArrayList no inicio, mas não é indicado | Se você souber o tamanho da lista é uma boa opção colocar no parametro ex.: = new ArrayList(300);
         List<String> listaNomes = new ArrayList(); // Depois de java 7 não precisa colocar <String> depois do new = new ArrayLista <String>();
         listaNomes.add("Livia");
         // agora se tentar colocar um numero dará erro. ex.: listaNomes.add(123);
-
         //Para ver o que tem na lista FOR EACH | agora pode colocar o tipo da variavel especifica ja que declarou ela no <> da Lista
+        System.out.println("add 1 item a lista");
+        for (String nomess :  listaNomes) {
+            System.out.println(nomess);
+        }
+
+        //pode add a qualquer momento um novo item na lista
+        listaNomes.add("Gianinni");
+        listaNomes.add("Gabrielle");
+        listaNomes.add("Java");
+
+        System.out.println();
+        System.out.println("add varios itens a lista:");
         for (String nomess :  listaNomes) {
             System.out.println(nomess);
         }
         System.out.println();
-        //pode add a qualquer momento um novo item na lista
-        listaNomes.add("Gianinni");
 
-        //PARA MOSTRAR O QUE TEM DENTRO DA LISTA FOR INDEXADO
+        // REMOVE
+        System.out.println("Removendo index 3");
+        listaNomes.remove(3);// deve remover o java
+        for (String nomess :  listaNomes) {
+            System.out.println(nomess);
+        }
+
+        System.out.println();
+        System.out.println("Removendo Nome Gabrielle");
+        listaNomes.remove("Gabrielle"); // se for identico a String, vai remover Gabrielle da lista
+        for (String nomess :  listaNomes) {
+            System.out.println(nomess);
+        }
+        System.out.println();
+        System.out.println("Lista por for iterado");
+        //PARA MOSTRAR O QUE TEM DENTRO DA LISTA USANDO FOR ITERADO - INDEXADO
         for (int i =0; i < listaNomes.size(); i++){
             System.out.println(listaNomes.get(i));
+        }
+        System.out.println();
+        // OBSERVAÇÕES SOBRE ADD AO ITERA - SE PRECISAR ADD ITEM NA LISTA DENTRO DO FOR
+        //Declara uma variavel chamada size e atribui a ela o tamanho da lista
+
+        System.out.println();
+        System.out.println("add item cass atraves do for");
+        int size = listaNomes.size(); // atribui a ela o tamanho da lista. assim não vai chamar o nome infinitamente.
+        for (int i =0; i < size; i++){
+            System.out.println(listaNomes.get(i));
+            listaNomes.add("Cass");
+        }
+        System.out.println(listaNomes);
+        //todavia essa moto não é uma boa pratica, melhor fazer por filtro
+
+        //ADICIONAR UMA LISTA EM OUTRO
+        System.out.println();
+        System.out.println("imagina que agora você tem 2 listas (lista abaixo):");
+        List<String> listaNomes2 = new ArrayList();
+        listaNomes2.add("William");
+        listaNomes2.add("Suane");
+
+        for (String nomess :  listaNomes2) {
+            System.out.println(nomess);
+        }
+        System.out.println();
+        listaNomes2.addAll(listaNomes);
+
+        System.out.println("Lista nomes 1 add a Lista nomes 2:");
+        for (String nomess :  listaNomes2) {
+            System.out.println(nomess);
         }
 
     }
